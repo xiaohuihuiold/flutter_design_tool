70abb649776f5885a879d0bdf0506eda58bcfab6
xiaohuihuiold <xiaohuihuiold@gmail.com>
2019-08-13 20:44:45 +0800

    :penguin: 添加linux支持

--- linux/flutter_design_tool.cc
+++ linux/flutter_design_tool.cc
@@ -11,31 +11,29 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <linux/limits.h>
+#include <unistd.h>
 
+#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <memory>
 #include <vector>
 
-#include "flutter/flutter_window_controller.h"
-
-// Include windows.h last, to minimize potential conflicts. The CreateWindow
-// macro needs to be undefined because it prevents calling
-// FlutterWindowController's method.
-#include <windows.h>
-#undef CreateWindow
+#include <flutter/flutter_window_controller.h>
 
 namespace {
 
 // Returns the path of the directory containing this executable, or an empty
 // string if the directory cannot be found.
 std::string GetExecutableDirectory() {
-  char buffer[MAX_PATH];
-  if (GetModuleFileName(nullptr, buffer, MAX_PATH) == 0) {
+  char buffer[PATH_MAX + 1];
+  ssize_t length = readlink("/proc/self/exe", buffer, sizeof(buffer));
+  if (length > PATH_MAX) {
     std::cerr << "Couldn't locate executable" << std::endl;
     return "";
   }
-  std::string executable_path(buffer);
-  size_t last_separator_position = executable_path.find_last_of('\\');
+  std::string executable_path(buffer, length);
+  size_t last_separator_position = executable_path.find_last_of('/');
   if (last_separator_position == std::string::npos) {
     std::cerr << "Unabled to find parent directory of " << executable_path
               << std::endl;
@@ -52,9 +50,9 @@ int main(int argc, char **argv) {
   if (base_directory.empty()) {
     base_directory = ".";
   }
-  std::string data_directory = base_directory + "\\data";
-  std::string assets_path = data_directory + "\\flutter_assets";
-  std::string icu_data_path = data_directory + "\\icudtl.dat";
+  std::string data_directory = base_directory + "/data";
+  std::string assets_path = data_directory + "/flutter_assets";
+  std::string icu_data_path = data_directory + "/icudtl.dat";
 
   // Arguments for the Flutter Engine.
   std::vector<std::string> arguments;
