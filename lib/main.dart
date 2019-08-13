07c154a7967cd10d6510233813ab728417e9dbb3
xiaohuihuiold <xiaohuihuiold@gmail.com>
2019-08-13 12:12:56 +0800

    初次提交

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'generated/i18n.dart';
+
+void main() {
+  _setTargetPlatformForDesktop();
+  runApp(MyApp());
+}
+
+/// 解决desktop错误
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FDT',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Alibaba-PuHuiTi',
+      ),
+      // 国际化
+      // S.of(context).xxx进行使用
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).appName),
+      ),
+    );
+  }
+}
