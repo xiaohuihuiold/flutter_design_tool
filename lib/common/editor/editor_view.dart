0442573a90c8d308eec4c5d185d05de30300f19c
xiaohuihuiold <xiaohuihuiold@gmail.com>
2019-08-21 14:56:19 +0800

    :sparkles: 添加快捷键

--- lib/common/editor/editor_view.dart
+++ lib/common/editor/editor_view.dart
@@ -1,6 +1,24 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
+
+import 'package:shortcut_keys/shortcut_keys.dart';
+
+/// 编辑器动作类型
+enum EditorViewActionType {
+  /// 普通
+  /// 鼠标滚轮只会使得画布上下移动
+  normal,
+
+  /// 移动画布
+  /// 鼠标或者手指操作画布上下左右移动
+  translate,
+
+  /// 缩放画布
+  /// 鼠标滚轮进行放大缩小
+  /// 手指拖动seek bar进行放大缩小
+  scale,
+}
 
 /// 编辑器视图
 /// 主要处理事件以及手势操作
@@ -10,64 +28,62 @@ class EditorView extends StatefulWidget {
 }
 
 class _EditorViewState extends State<EditorView> {
+  /// 编辑器焦点
+  /// 拥有焦点则可以使用快捷键
+  FocusNode _editorFocus = FocusNode();
 
-  /// 键盘事件
-  void _handleRawKeyEvent(RawKeyEvent event) {
-    print(event);
+  /// 当鼠标进入编辑器区域时
+  void _onMouseEnter(PointerEnterEvent event) {
+    // 注册编辑器的焦点
+    FocusScope.of(context).requestFocus(_editorFocus);
+  }
+
+  /// 当鼠标退出编辑器区域时
+  void _onMouseExit(PointerExitEvent event) {
+    // 注册一个新的焦点
+    // 使得旧的焦点失效
+    FocusScope.of(context).requestFocus(FocusNode());
   }
 
   @override
   void initState() {
     super.initState();
-    RawKeyboard.instance.addListener(_handleRawKeyEvent);
   }
 
   @override
   void dispose() {
-    RawKeyboard.instance.removeListener(_handleRawKeyEvent);
     super.dispose();
+  }
+
+  List<ShortcutData> test() {
+    return [
+      ShortcutData(
+        shortcuts: [ShortcutKeys.CTRL_LEFT, ShortcutKeys.C],
+        trigger: () {
+          print('复制');
+        },
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MouseRegion(
-          onHover: (event) {
-            // 鼠标悬浮
-            print(event);
+    return ShortcutKeysListener(
+      focusNode: _editorFocus,
+      onMouseEnter: _onMouseEnter,
+      onMouseExit: _onMouseExit,
+      shortcutData: [
+        ...test(),
+        ShortcutData(
+          shortcuts: [ShortcutKeys.CTRL_LEFT, ShortcutKeys.S],
+          trigger: () {
+            print('保存');
           },
-          onEnter: (event) {
-            // 鼠标指针进入区域时
-            print(event);
-          },
-          onExit: (event) {
-            // 鼠标指针退出区域时
-            print(event);
-          },
-          child: Listener(
-            onPointerSignal: (event) {
-              // 滚轮信号
-              print(event);
-            },
-            onPointerMove: (event) {
-              // 点击后移动
-              print(event);
-            },
-            onPointerDown: (event) {
-              // 左键(鼠标)按下
-              print(event);
-            },
-            onPointerCancel: (event) {
-              // 取消
-              print(event);
-            },
-            onPointerUp: (event) {
-              // 抬起
-              print(event);
-            },
-            child: _EditorSketch(),
-          ),
         ),
+      ],
+      child: Container(
+        child: _EditorSketch(),
+      ),
     );
   }
 }
